8d8cd879b943d1ac112d3fcb2107078efb3cdef2
Yulin Liu <yl4758@nyu.edu>
2019-10-06 20:14:51 -0400

    Updates

---
+++ precache-manifest.96359d9bd91f8ed84a62c25c0289f60e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9dc0a240d690872dbb0d58f590520b20",
+    "url": "/myContactsList/index.html"
+  },
+  {
+    "revision": "c1cb7c84f0a6b58099b6",
+    "url": "/myContactsList/static/css/main.0031414c.chunk.css"
+  },
+  {
+    "revision": "09985cce42a50685b978",
+    "url": "/myContactsList/static/js/2.3cafb898.chunk.js"
+  },
+  {
+    "revision": "c1cb7c84f0a6b58099b6",
+    "url": "/myContactsList/static/js/main.edf32feb.chunk.js"
+  },
+  {
+    "revision": "afe5e62e2bda925577af",
+    "url": "/myContactsList/static/js/runtime-main.36c15698.js"
+  }
+]);
